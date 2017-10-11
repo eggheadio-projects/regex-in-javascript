51cfea4189332438410edab2c8a56f82daa32c0b
Zac Jones <zacjones93@gmail.com>
2017-10-11 14:12:51 -0700

    updated console.log behavior

--- script.js
+++ script.js
@@ -12,21 +12,11 @@ var regex = /is/gi;
 
 output(str, regex, document.querySelector('pre'))
 
-// console.log(regex.test(str)) || displayInPreview(regex.test(str));
-// console.log(regex.exec(str)) || displayInPreview(regex.exec(str));
-// console.log(regex.exec(str)) || displayInPreview(regex.exec(str));
-// console.log(regex.exec(str)) || displayInPreview(regex.exec(str));
-// console.log(regex.exec(str)) || displayInPreview(regex.exec(str));
-// console.log(str.match(regex)) || displayInPreview(sr.match(regex));
-// console.log(str.replace(regex, str => "XX")) || displayInPreview(regex, str => "XX");
-// console.log(str.search(regex)) || displayInPreview(str.search(regex));
-
-
-
-// display in plunker preview
-function displayInPreview(string) {
-  var newDiv = document.createElement("div"); 
-  var newContent = document.createTextNode(string); 
-  newDiv.appendChild(newContent);
-  document.body.appendChild(newDiv)
-}
+// console.log(regex.test(str));
+// console.log(regex.exec(str)); 
+// console.log(regex.exec(str)); 
+// console.log(regex.exec(str)); 
+// console.log(regex.exec(str)); 
+// console.log(str.match(regex)); 
+// console.log(str.replace(regex, str => "XX")); 
+// console.log(str.search(regex)); 
